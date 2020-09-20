e2d149dab48d86f98ccdaa7317e54f89fab61ed6
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-20 15:57:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  name_hash.each_value do |min|
+    if  min < name_hash[min]
+      min = name_hash[min]
+    end
+    min += 1 
+  end
 
 end
