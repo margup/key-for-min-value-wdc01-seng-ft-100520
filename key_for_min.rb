4eb38d660f747ab7c684d8a8b39530c638935fdf
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-20 16:00:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,22 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  key_for_min_value = nil
-  min_value = nil 
-  name_hash.each_value do |key,value|
-if 
-    end
-    min += 1 
-  end
+	def key_for_min_value(name_hash)
 
-end
+  current_minimum_val = nil 
+  current_key_assoc_w_min_val = nil 
+
+  name_hash.each do |key, value|
+
+    if current_minimum_val == nil 
+      current_minimum_val = value 
+      current_key_assoc_w_min_val = key 
+    else 
+ 
+      if current_minimum_val > value 
+        current_minimum_val = value
+        current_key_assoc_w_min_val = key 
+      end
+    end
+  end 
+  current_key_assoc_w_min_val
